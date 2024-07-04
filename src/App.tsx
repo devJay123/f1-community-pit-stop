969c31a6528aa23357eb7af7e09c9d94d9ce497d
JaeHyeok <morest432@gmail.com>
2024-07-04 17:03:01 +0900

    feat: 글작성 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import "./common.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Header";
-import BoardApp from "./board/BoardApp";
-import BoardList from "./board/BoardList";
-import BoardForm from "./board/BoardForm";
-import BoardEdit from "./board/BoardEdit";
-import BoardReply from "./board/BoardReply";
+import './App.css';
+import './common.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import BoardApp from './board/BoardApp';
+import BoardList from './board/BoardList';
+import BoardForm from './board/BoardForm';
+import BoardEdit from './board/BoardEdit';
+import BoardReply from './board/BoardReply';
 
 // temp로 써져있는거 삭제하기
 function App() {
@@ -17,10 +17,10 @@ function App() {
         <Routes>
           <Route path="/community" element={<BoardApp />} />
           <Route path="/community/list/:id" element={<BoardList />} />
-          <Route path="templist" element={<BoardList />} />
-          <Route path="tempform" element={<BoardForm />} />
-          <Route path="tempedit" element={<BoardEdit />} />
-          <Route path="tempreply" element={<BoardReply />} />
+          <Route path="/templist" element={<BoardList />} />
+          <Route path="/tempform" element={<BoardForm />} />
+          <Route path="/tempedit" element={<BoardEdit />} />
+          <Route path="/tempreply" element={<BoardReply />} />
         </Routes>
       </BrowserRouter>
     </div>
