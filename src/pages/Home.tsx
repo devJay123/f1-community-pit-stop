d175bed3d1a5c053ae373aa2ab9a85037a999137
JaeHyeok <morest432@gmail.com>
2024-07-06 14:28:14 +0900

    feat: home 화면 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,17 @@
-import React from "react";
-import { Carousel } from "react-bootstrap";
+import React from 'react';
+import './home.css';
+import { Carousel, Container } from 'react-bootstrap';
 export default function Hone() {
   return (
-    <Carousel>
-      ddd
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="First slide" /> */}
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="Second slide" /> */}
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="main-banner">
+      <Carousel>
+        <Carousel.Item>
+          <div className="slide_item banner1"></div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="slide_item banner2"></div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
