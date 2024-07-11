63f5e52843eba1f3cab3f5bf3b2a11edc959a370
YOUNGBINOK <redmangos@naver.com>
2024-07-11 22:02:16 +0900

    feat:rank import React 삭제

--- src/pages/Rank.tsx
+++ src/pages/Rank.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Row, Col, Card, Container, Badge } from "react-bootstrap";
 import profile from "../lib/driverProfile";
