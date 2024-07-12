c001ad71c85ba50ea63d5cd7e62d9c13694889b0
YOUNGBINOK <redmangos@naver.com>
2024-07-12 13:27:57 +0900

    fix: BoardApp 수정

--- src/board/BoardApp.tsx
+++ src/board/BoardApp.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col, Card, Alert } from "react-bootstrap";
 import teams from "../lib/teamInfo";
+
 import "./BoardApp.css";
 
 interface ITeam {
--- src/lib/teamInfo.js
+++ src/lib/teamInfo.js
@@ -20,6 +20,28 @@ import alpineLogo from "../assets/team_logo/alpine-logo.png";
 import williamsLogo from "../assets/team_logo/williams-logo.png";
 import sauberLogo from "../assets/team_logo/kick-sauber-logo.png";
 
+import maxver01 from "../assets/player/maxver01.png";
+import alealb01 from "../assets/player/alealb01.png";
+import carsai01 from "../assets/player/carsai01.png";
+import chalec01 from "../assets/player/chalec01.png";
+import estoco01 from "../assets/player/estoco01.png";
+import feralo01 from "../assets/player/feralo01.png";
+import georus01 from "../assets/player/georus01.png";
+import guazho01 from "../assets/player/guazho01.png";
+import kevmag01 from "../assets/player/kevmag01.png";
+import lannor01 from "../assets/player/lannor01.png";
+import lanstr01 from "../assets/player/lanstr01.png";
+import lewham01 from "../assets/player/lewham01.png";
+import logsar01 from "../assets/player/logsar01.png";
+import nichul01 from "../assets/player/nichul01.png";
+//import olibea01 from "../assets/player/olibea01.png";
+import oscpia01 from "../assets/player/oscpia01.png";
+import valbot01 from "../assets/player/valbot01.png";
+import yuktsu01 from "../assets/player/yuktsu01.png";
+import danric01 from "../assets/player/danric01.png";
+import serper01 from "../assets/player/serper01.png";
+import piegas01 from "../assets/player/piegas01.png";
+
 const teams = [
   {
     id: 1,
@@ -27,9 +49,9 @@ const teams = [
     rank: 1,
     logo: redbullLogo,
     driverName1: "Max VERSTAPPEN",
-    driverProfile1: "./src/assets/player/maxver01.png",
+    driverProfile1: maxver01,
     driverName2: "Sergio PEREZ",
-    driverProfile2: "./src/assets/player/serper01.png",
+    driverProfile2: serper01,
     imageUrl: redbull,
     teamColor: "rgb(54 113 198 / 1)",
   },
@@ -39,9 +61,9 @@ const teams = [
     rank: 2,
     logo: ferrariLogo,
     driverName1: "Charles LECLERC",
-    driverProfile1: "./src/assets/player/chalec01.png",
+    driverProfile1: chalec01,
     driverName2: "Carlos SAINZ",
-    driverProfile2: "./src/assets/player/carsai01.png",
+    driverProfile2: carsai01,
     imageUrl: ferrari,
     teamColor: "rgb(232 0 32 /1)",
   },
@@ -51,9 +73,9 @@ const teams = [
     rank: 3,
     logo: mclarenLogo,
     driverName1: "Lando NORRIS",
-    driverProfile1: "./src/assets/player/lannor01.png",
+    driverProfile1: lannor01,
     driverName2: "Oscar PIASTRI",
-    driverProfile2: "./src/assets/player/oscpia01.png",
+    driverProfile2: oscpia01,
     imageUrl: mclaren,
     teamColor: "rgb(255 128 0 /1)",
   },
@@ -63,9 +85,9 @@ const teams = [
     rank: 4,
     logo: mercedesLogo,
     driverName1: "Lewis HAMILTON",
-    driverProfile1: "./src/assets/player/lewham01.png",
+    driverProfile1: lewham01,
     driverName2: "George RUSSELL",
-    driverProfile2: "./src/assets/player/georus01.png",
+    driverProfile2: georus01,
     imageUrl: mercedes,
     teamColor: "rgb(39 244 210/1)",
   },
@@ -75,9 +97,9 @@ const teams = [
     rank: 5,
     logo: astonmartinLogo,
     driverName1: "Fernando ALONSO",
-    driverProfile1: "./src/assets/player/feralo01.png",
+    driverProfile1: feralo01,
     driverName2: "Lance STROLL",
-    driverProfile2: "./src/assets/player/lanstr01.png",
+    driverProfile2: lanstr01,
     imageUrl: astonmartin,
     teamColor: "rgb(34 153 113/1)",
   },
@@ -87,9 +109,9 @@ const teams = [
     rank: 6,
     logo: rbLogo,
     driverName1: "Yuki TSUNODA",
-    driverProfile1: "./src/assets/player/yuktsu01.png",
+    driverProfile1: yuktsu01,
     driverName2: "Daniel RICCIARDO",
-    driverProfile2: "./src/assets/player/danric01.png",
+    driverProfile2: danric01,
     imageUrl: rb,
     teamColor: "rgb(102 146 255/1)",
   },
@@ -99,9 +121,9 @@ const teams = [
     rank: 7,
     logo: haasLogo,
     driverName1: "Nico HULKENBERG",
-    driverProfile1: "./src/assets/player/nichul01.png",
+    driverProfile1: nichul01,
     driverName2: "Kevin MAGNUSSEN",
-    driverProfile2: "./src/assets/player/kevmag01.png",
+    driverProfile2: kevmag01,
     imageUrl: haas,
     teamColor: "rgb(182 186 189 /1)",
   },
@@ -111,9 +133,9 @@ const teams = [
     rank: 8,
     logo: alpineLogo,
     driverName1: "Pierre GASLY",
-    driverProfile1: "./src/assets/player/piegas01.png",
+    driverProfile1: piegas01,
     driverName2: "Esteban OCON",
-    driverProfile2: "./src/assets/player/estoco01.png",
+    driverProfile2: estoco01,
     imageUrl: alpine,
     teamColor: "rgb(0 147 204 / 1)",
   },
@@ -123,9 +145,9 @@ const teams = [
     rank: 9,
     logo: williamsLogo,
     driverName1: "Alexander ALBON",
-    driverProfile1: "./src/assets/player/alealb01.png",
+    driverProfile1: alealb01,
     driverName2: "Logan SARGEANT",
-    driverProfile2: "./src/assets/player/logsar01.png",
+    driverProfile2: logsar01,
     imageUrl: williams,
     teamColor: "rgb(100 196 255  / 1)",
   },
@@ -135,9 +157,9 @@ const teams = [
     rank: 10,
     logo: sauberLogo,
     driverName1: "Valtteri BOTTAS",
-    driverProfile1: "./src/assets/player/valbot01.png",
+    driverProfile1: valbot01,
     driverName2: "ZHOU Guanyu",
-    driverProfile2: "./src/assets/player/guazho01.png",
+    driverProfile2: guazho01,
     imageUrl: sauber,
     teamColor: "rgb(82 226 82 /1)",
   },
