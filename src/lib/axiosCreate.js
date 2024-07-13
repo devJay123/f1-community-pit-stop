e7db223eb61167740aad1d8727898114ca9daeea
YOUNGBINOK <redmangos@naver.com>
2024-07-13 17:26:50 +0900

    feat:BoardList 검색 추가

--- src/lib/axiosCreate.js
+++ src/lib/axiosCreate.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    'https://port-0-f1-community-pit-stop-server-ly5p232t0b63615b.sel5.cloudtype.app', // proxy설정과 동일하게 baseURL(기본 URL)값 설정
+  // baseURL:
+  //   "https://port-0-f1-community-pit-stop-server-ly5p232t0b63615b.sel5.cloudtype.app", // proxy설정과 동일하게 baseURL(기본 URL)값 설정
+  baseURL: "http://localhost:8000",
 });
 
 export default instance;
